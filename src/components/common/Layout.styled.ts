b1bae567d8fa0307f7fb40b1f8dc4607c670242c
seoro <sho0814@hanyang.ac.kr>
2025-08-25 01:11:14 +0900

    feat:카메라 완성

--- src/components/common/Layout.styled.ts
+++ src/components/common/Layout.styled.ts
@@ -5,17 +5,9 @@ export const LayoutContainer = styled.div<{ noVerticalPadding?: boolean }>`
   min-width: 360px;
   max-width: 400px;
   height: 100vh;
-  background: #f6f7f9;
-<<<<<<< HEAD
+  background: #F7F8F9;
   padding-top: ${({ noVerticalPadding }) => (noVerticalPadding ? '0' : '49px')};
   padding-bottom: ${({ noVerticalPadding }) => (noVerticalPadding ? '0' : '49px')};
   margin: 0 auto; /* 가로 가운데 정렬 */
-=======
-  padding-top: 49px;
-  padding-bottom: 95px;
-  padding-left: 22px;
-  padding-right: 22px;
-  margin: 0 auto;       /* 가로 가운데 정렬 */
->>>>>>> develop
   overflow: auto;
 `;

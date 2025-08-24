0d6ddde0f69613fac40b2958f6c7682be6a46d09
seoro <sho0814@hanyang.ac.kr>
2025-08-24 18:44:22 +0900

    충돌해결

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.myapp.com", // 공통 URL
+  baseURL: "https://43.203.226.243", // 공통 URL
   headers: {
     "Content-Type": "application/json",
   },

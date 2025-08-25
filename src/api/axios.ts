09f65e50f44fc9d35a7826f6222dba2872896960
kkeunii <greenlee55@naver.com>
2025-08-25 23:29:40 +0900

    fix: 왜 안되지

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,10 +2,14 @@
 import axios from "axios";
 
 // 개발 단계용 고정 토큰 (나중엔 로그인 인증 결과로 대체)
+const ACCESS_TOKEN = "cc25e466-fc6c-4d24-8a63-6f3cba41cbd0";
 
 // 공통 axios 인스턴스
 const apiClient = axios.create({
-  baseURL: 'https://pickle-back.o-r.kr', // 실제 서버 주소 작성
+  baseURL: "https://pickle-back.o-r.kr", // 실제 서버 주소 작성
+  headers: {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
 });
 
 export default apiClient;
